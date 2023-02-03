3ecd9a868025b4e6e7629a71ee05b8b934897186
m.kahtooei <m.kahtooei@gmail.com>
2023-02-03 13:24:49 +0330

    start project

---
+++ android/app/src/main/kotlin/com/kahtooei/cab_rider/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kahtooei.cab_rider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:cab_rider/UI/Screens/main_screen/main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
