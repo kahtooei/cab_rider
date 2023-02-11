5e38939066cc421b988af8a23b1027083c83a9ad
m.kahtooei <m.kahtooei@gmail.com>
2023-02-11 13:19:32 +0330

    main page ui

--- lib/UI/Screens/main/main_screen.dart
+++ lib/UI/Screens/main/main_screen.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:async';
@@ -19,20 +18,98 @@ class _MainScreenState extends State<MainScreen> {
     zoom: 14.4746,
   );
 
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            myLocationButtonEnabled: true,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            left: 0,
+            child: Container(
+              height: 300,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black45,
+                    blurRadius: 15,
+                    spreadRadius: 0.8,
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 24, vertical: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Nice to see you",
+                      style: TextStyle(fontSize: 12),
+                    ),
+                    const Text(
+                      "Where are you going?",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(5),
+                          boxShadow: const [
+                            BoxShadow(
+                                blurRadius: 4,
+                                spreadRadius: 0.5,
+                                color: Colors.black26)
+                          ]),
+                      child: const TextField(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.search),
+                          hintText: "search destination",
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.home),
+                      title: const Text("Add Home"),
+                      subtitle: const Text("Your residential address"),
+                      onTap: () {},
+                    ),
+                    const Divider(
+                      color: Colors.black38,
+                      height: 1,
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.work),
+                      title: const Text("Add Work"),
+                      subtitle: const Text("Your office address"),
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
