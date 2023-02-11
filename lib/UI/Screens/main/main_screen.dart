d11a587d0d1d74596dc1a774a272a22936bb0973
m.kahtooei <m.kahtooei@gmail.com>
2023-02-11 11:26:08 +0330

    add google maps to project

--- lib/UI/Screens/main/main_screen.dart
+++ lib/UI/Screens/main/main_screen.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -9,25 +11,28 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  int counter = 1;
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static const CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Main Screen"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            print("Start");
-            DatabaseReference ref =
-                FirebaseDatabase.instance.ref("Test-$counter");
-            await ref.set({"value": "My Value For Test"});
-            print("End");
-            counter++;
-          },
-          child: const Text("Add Text To Firebase Database"),
-        ),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
     );
   }
